80f28ceeca766747dce2747695ed72a65333e7ea
Caio Gomes <caio@aponta-spa.(none)>
2013-01-03 17:52:27 -0200

    Adding Apontador Report

--- Reports/Apontador/ApontadorReport.py
+++ Reports/Apontador/ApontadorReport.py
@@ -1,5 +1,16 @@
 import Dev.DB.Oracle
 
-def GenerateReports():
-	db = Dev.DB.Oracle.oracle()
-		
+class GenerateReport(object):
+	def __init__(self):
+		self.db = Dev.DB.Oracle.oracle()
+	
+	def QueryLastMonth(self,time,rownum):
+		query = 'select * from (select lbs_id,sum(visit) as visits,sum(phone) from CONTABILIZAR.tbl_mongo_sumarizado where data>\'{data}\' group by lbs_id order by visits desc) where rownum <= {rownum} order by visits desc'
+		cursor = db.query(query.format('data'=data,'rownum'=rownum)
+		dados = cursor.fetchall()
+		for linha in dados:
+			
+			
+
+	def QueryYearPlusOne(self,time):
+	
